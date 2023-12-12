6b4601bd2732c3b1d2e0c4bc2ca0e88b229b880d
Julio Alves da Silva Jr <julioadsj@outlook.com>
2023-12-12 18:24:45 -0300

    Exercicio 110

---
+++ Exercicios/Desafio110.py
@@ -0,0 +1,8 @@
+"""Desafio 110: Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui."""
+
+from Modulos import moeda
+
+p=float(input("Digite o preço: R$"))
+moeda.resumo(p,20,12)
+
+
--- Exercicios/Modulos/moeda.py
+++ Exercicios/Modulos/moeda.py
@@ -3,6 +3,10 @@ Faça também um programa que importe esse modulo e use algumas dessas funções
 
 """Desafio 108: Adapte o código do Desafio 107, criando uma função adicional chamada moeda() que consiga mostrar os valores como um valor monetario formatado."""
 
+"""Desafio 109: Modifique as funções que foram criadas no Desafio 107 para que elas aceitem um parametro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no Desafio 108"""
+
+"""Desafio 110: Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui."""
+
 #Função que irá aumentar o valor digitado de acordo com a taxa
 def aumentar(preco = 0,taxa = 0,f=True):
     res=preco+(preco*taxa/100)
@@ -46,3 +50,21 @@ def metade(preco = 0,f=True):
 #Função que irá formatar o valor inserido para o formato de moeda
 def moeda(preco = 0,moeda = "R$",f=True):    
     return f"{moeda}{preco:.2f}".replace(".",",")
+
+#Função que irá chamar de todas as funções criadas até agora de forma resumida
+def resumo(preco=0,taxaAumento=0,taxaReducao=0):
+    print("-"*35)
+    print("Resumo do valor".center(30))
+    print("-"*35)
+
+    print(f"Preço analisado: \t{moeda(preco)}")
+    print(f"Dobro do preço: \t{dobro(preco,True)}")
+    print(f"Metade do preço: \t{metade(preco,True)}")
+    print(f"Com {taxaAumento}% de aumento: \t{aumentar(preco,taxaAumento,True)}")
+    print(f"Com {taxaReducao}% de redução: \t{diminuir(preco,taxaReducao,True)}")
+
+    print("-"*35)
+    
+    
+
+    
