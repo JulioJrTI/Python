8dc869f4fa261fdff63eaf56b5fd32818dda3692
Julio Alves da Silva Jr <julioadsj@outlook.com>
2023-12-24 15:42:42 -0300

    Exercicio115 - Concluido

--- Exercicios/Desafio115/sistema.py
+++ Exercicios/Desafio115/sistema.py
@@ -1,16 +1,28 @@
 from Desafio115.lib.interface import *
+from Desafio115.lib.arquivo import *
 from time import sleep
 
+arq = "CursoEmVideo.txt"
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 while True:
-    resposta = menu(["Ver pessoas cadastradas","Cadastrar nova pessoa","Sair do Sistema"])
-    if resposta==1:
-        cabecalho("Opção 1")
-    elif resposta==2:
-        cabecalho("Opção 2")
-    elif resposta==3:
+    resposta = menu(["Ver pessoas cadastradas", "Cadastrar nova pessoa", "Sair do Sistema"])
+    if resposta == 1:
+        #Opção de listar o conteudo de um arquivo
+        lerArquivo(arq)
+    elif resposta == 2:
+        #Opção para cadastrar uma pessoa
+        cabecalho("NOVO CADASTRO")
+        nome = str(input("Nome: "))
+        idade = leiaInt("Idade: ")
+        cadastrar(arq,nome,idade)
+    elif resposta == 3:
         cabecalho("Opção 3")
         cabecalho("Saindo do sistema...")
         break
     else:
         print("Erro! Digite uma opção valida!")
     sleep(2)
+
---
+++ Exercicios/TreinamentoPython.py
@@ -0,0 +1,3 @@
+lista = [2,2,5,7,9,6,1,23]
+
+print(lista.count(2))
