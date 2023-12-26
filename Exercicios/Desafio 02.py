ceef730391f2c9b407f081bd4d121f23dfc38485
Julio Alves da Silva Jr <julioadsj@outlook.com>
2023-12-26 20:40:42 -0300

    Exercicio 02 - Usando FUNÇÕES, LOOPS WHILE e FOR, CONDIÇÕES, LISTAS e TRATAMENTOS DE ERROS.

--- Exercicios/Desafio 02.py
+++ Exercicios/Desafio 02.py
@@ -1,20 +1,62 @@
 """Desafio 02: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vidas."""
 
-color1 = "\033[1;32m"
-color2 = "\033[1;33m"
-color3 = "\033[1;31m"
-color4 = "\033[1;34m"
-colorEnd = "\033[m"
+from Modulos import Formatar
+from time import sleep
 
-pessoas = []  # Iremos colocar o nomes das pessoas nesta lista
+homensLista=[]
+mulheresLista=[]
 
-print ( "=+" * 10, f"{color1}Bem vindo a festinha da Alexa!{colorEnd}", "=+" * 10 )
-while True :
-    pessoas.append ( input ( f"{color2}Digite o nome da garota:{colorEnd} " ) )
-    c = input ( f"{color3}Deseja continuar? [S/N]{colorEnd}" )
+print(Formatar.cor(Formatar.cabecalho("Bem vindo ao cadastrador de pessoas da Alexa!",35),35))
+while True:
+    while True:
+        nomePessoa=str(input("Digite seu nome: (Ou digite exit para sair)\n"))
+        if nomePessoa.isalpha():
+            break
+        else:
+            print(Formatar.cor("Erro! Por favor digite um nome!",31))
 
-    if c in "Nn" :
+    if nomePessoa=="exit":
+        print("Finalizando o programa...")
+        sleep(0)#1        
+        print(Formatar.cor(Formatar.cabecalho("Obrigado e volte sempre!"),32))
+        sleep(0)#1     
         break
+    else:
+        print(Formatar.cabecalho(f"Cadastrando {nomePessoa}..."))
+        
+        sleep(0)#2
+        
+        if nomePessoa[-1]=="a":
+            c=str(input("Vc é uma mulher? [S/N] "))
+            if c in "Ss":
+                print(Formatar.cor(f"{nomePessoa}! Seja bem vinda ao Python!",35))        
+                mulheresLista.append(nomePessoa)
+            elif c in "Nn":
+                print(Formatar.cor(f"{nomePessoa}, Bem vindo ao Python!",34))
+                homensLista.append(nomePessoa)
+        else:
+            print(Formatar.cor(f"{nomePessoa}, Bem vindo ao Python!",34))
+            homensLista.append(nomePessoa)
+        
+        sleep(0)#2
 
-for p in pessoas :  # Iremos falar o nomes das pessoas inseridas na lista e iremos dar boas vindas a elas
-    print ( f"{color4}Bem vinda {p}!{colorEnd}" )
+print(Formatar.cor(Formatar.cabecalho("Exibindo agora a lista de pessoas cadastradas: "),35))
+
+print(Formatar.cor("Homens cadastrados:",34))
+if len(homensLista)>=1:
+    for i,h in enumerate(homensLista):
+        print(f"{i}: {h}")
+else:
+    print("Não foram cadastrados homens!")
+    
+print()
+
+print(Formatar.cor("Mulheres cadastradas:",35))
+if len(mulheresLista)>=1:
+    for i,m in enumerate(mulheresLista):
+        print(f"{i}: {m}")
+else:
+    print("Não foram cadastrados mulheres!")
+    
+Formatar.linhas()
+
--- Exercicios/Modulos/Formatar.py
+++ Exercicios/Modulos/Formatar.py
@@ -1,6 +1,7 @@
 #Função que irá colorizar um texto
 def cor(texto,num=32):
     """
+    \033[1;33m
     Bem vindo ao colorizador da Alexa!
     Para usarmos essa função, siga o exemplo de um codigo:
     
@@ -14,16 +15,33 @@ def cor(texto,num=32):
     
     A cor padrão é 32 (Verde).
     
+    Opções de Cores:
+    31: Vermelho
+    32: Verde
+    33: Amarelo
+    34: Azul
+    35: Magenta
+    36: Ciano
+    37: Branco
+    
+    \033[m
     """   
     return f"\033[1;{num}m{texto}\033[m" 
 
 
 #Função que irá adicionar linhas
 def linhas(quant=30):
-    return "-"*quant
+    """
+    Para imprimir caracteres, insira o mesmo deseja como o primeiro modulo e em seguinda digite a quantidade de caracteres que iremos imprimir.
+    
+    O caracteres padrão é "*".
+    A quantidade padrão é 30.
+    
+    """    
+    return "*" *quant
 
 
 #Função que irá centralizar um texto e inserir linhas acima e abaixo do mesmo
-def cabecalho(texto,center=55):
-    resultado = f"{linhas(len(texto))}\n{texto.center(center)}\n{linhas(len(texto))}"    
+def cabecalho(texto,cent=0):
+    resultado = f"{linhas(len(texto))}\n{texto.center(cent)}\n{linhas(len(texto))}"    
     return resultado
