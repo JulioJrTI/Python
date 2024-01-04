5ed527556f502aa96842382415a332ef9a2d5a76
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-01-04 20:44:08 -0300

    Exercicio 05 - Usando LISTAS, LOOPS WHILE e FOR e CONDIÇÕES

--- Exercicios/Desafio 05.py
+++ Exercicios/Desafio 05.py
@@ -1,27 +1,62 @@
 """Desafio 05: Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor."""
 
-from time import sleep
+import os
+from Modulos.Formatar import cabecalho,cor
 
-color1 = "\033[1;32m"
-color2 = "\033[1;33m"
-colorEnd = "\033[m"
+#Limpando o terminal a cada execução do programa
+os.system("cls")
 
-maior = 0
-menor = 0
+#Iremos armazenar os numeros digitados em uma lista
+numeros=[] 
 
-for n in range ( 5 ) :
-    numero = int ( input ( f"Digite o {n + 1}º numero: " ) )
-    print ( f"{color2}O numero {numero} vem antes do numero {numero + 1} e depois do numero {numero - 1}.{colorEnd}" )
-    sleep ( 1 )
+#Chave para verificarmos numeros ou finalizar o programa
+chave=True 
 
-    if n == 0 :  # Iremos verificar quais foram os maiores e menores numeros digitados pelo input
-        maior = numero
-        menor = numero
-    else :
-        if numero > maior :
-            maior = numero
-        if numero < menor :
-            menor = numero
-
-print ( f"{color1}O maior numero digitado foi o {maior}.{colorEnd}" )
-print ( f"{color2}O menor numero digitado foi o {menor}.{colorEnd}" )
+#Programa principal
+cabecalho(cor("Bem vindo ao analisador de numeros da Alexa!"),63)
+while True:
+    #Perguntando ao usuario a quantidade de numeros que iremos armazenar em uma lista
+    while True:
+        try:    
+            quantNum = int(input("Deseja digitar quantos numeros: ")) 
+            break
+        except KeyboardInterrupt: #Caso o usuario cancele o programa no meio de sua execução
+            print(cor("Ok, volte sempre!",31))
+            quit()
+        except: #Caso o usuario digite algo alem de numeros
+            print(cor("Por favor digite um valor numerico valido!",31))        
+        
+    #Perguntando os numeros de acordo com a quantidade desejada acima
+    while True:
+        try:
+            for n in range(quantNum): 
+                numeros.append(int(input(f"Digite o {n}º numero: ")))
+            print()
+            break    
+        except KeyboardInterrupt: #Caso o usuario cancele o programa no meio de sua execução
+            print(cor("Ok, volte sempre!",31))
+            quit()        
+        except: #Caso o usuario digite algo alem de numeros, e tambem iremos limpar a lista a cada erro
+            numeros.clear()
+            print(cor("Erro! Digite um numero valido!",31))
+        
+    #Lista de numeros digitados
+    cabecalho(cor("Segue lista de numeros digitados:",33),53)
+    for i, n in enumerate(numeros):
+            print(f"{i}º = {n}")    
+    
+    while chave: #Chave que irá controlar se o usuario deseja verificar mais do que um numero ou sair do programa
+        print()        
+        c=int(input(f"Deseja verificar qual numero: (Ou digite {cor(len(numeros),31)} para sair)"))
+        print()
+        
+        if c == len(numeros): #Se o usuario digitar o numero escolhido acima para sair do programa
+            print(cor("Obrigado e volte sempre!",35))
+            chave=False            
+        elif c > len(numeros): #Se o usuario digitar um numero (alem do numero de saida) que esteja fora do range da lista
+            print(cor(f"Erro! Escolha somente do numero 0 até {len(numeros)-1}",31))                
+        else: #Caso entrada esteja correta, iremos exibir o resultado do numero desejado        
+            cabecalho(cor(f"O numero {numeros[c]} tem como sucessor o numero {numeros[c]+1} e antecessor o numero {numeros[c]-1}!",34),0)
+    
+    #Saida do programa        
+    break 
