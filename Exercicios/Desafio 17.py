3401257455b496a1ca6cb44997f3ac7f705b545c
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-08 17:00:29 -0300

    Exercicio 17 - Usando FUNÇÕES, LOOPS WHILE, TRATAMENTOS DE ERRO, VARIAVEIS e CONDIÇÕES

--- Exercicios/Desafio 17.py
+++ Exercicios/Desafio 17.py
@@ -1,62 +1,67 @@
-"""Desafio 17: Faça um programa que leia o comprimento do cateto oposto 
-e do cateto adjacente de um triangulo retângulo, calcule e mostre o comprimento da hipotenusa."""
+"""Desafio 17: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retângulo, calcule e mostre o comprimento da hipotenusa."""
 
+#Usando de funções de matematica
 import math
 
-color1="\033[1;32m"
-color2="\033[1;33m"
-color3="\033[1;34m"
-colorEnd="\033[m"
+#Usando de funções de formatação
+from Modulos.Formatar import cabecalho,cor
 
-trianguloRetangulo=[[],[],[]]
+#Limpando o terminal a cada execução do programa
+from os import system
+system("cls")
 
-c2=False
+#Greetings!
+cabecalho(cor("Bem vindo ao calculador de Hipotenusa da Prof(a) Alexa!",35))
 
+#Programa principal
 while True:
-    co=float(input(f"{color1}Digite o cateto oposto: {colorEnd}"))
-    trianguloRetangulo[0].append(co)
+    #Tratamento de erros
+    while True:
+        try:    
+            #Solicitando um valor float para o Cateto Oposto
+            ca = float(input("Digite um valor para o Cateto Oposto: "))
+            break
+        except ValueError:
+            print(cor("Erro! Digite somente valores numericos! (Exemplo 4)",31))
+        except KeyboardInterrupt:
+            print(cor("\nO usuario cancelou o programa...",31))
+            quit()
+
+    #Tratamento de erros
+    while True:
+        try:
+            #Solicitando um valor float para o Cateto Adjacente
+            co = float(input("Digite um valor para o Cateto Adjacente: "))
+            break
+        except ValueError:
+            print(cor("Erro! Digite somente valores numericos! (Exemplo 3)",31))
+        except KeyboardInterrupt:
+            print(cor("\nO usuario cancelou o programa...",31))
+            quit()
+
+    #Calculando o valor do Cateto Oposto ao quadrado
+    caQuadrado = (ca**2)
+
+    #Calculando o valor do Cateto Adjacente ao quadrado
+    coQuadrado = (co**2)
+
+    #Somando os resultados das variaveis acima
+    somaCaCo = (caQuadrado+coQuadrado)
+
+    #Calculando a Hipotenusa ao calcular a Raiz Quadrada do valor acima
+    hipo=math.sqrt(somaCaCo)
     
-    ca=float(input(f"{color2}Digite o cateto adjacente: {colorEnd}"))
-    trianguloRetangulo[1].append(ca)
+    #Imprimindo o resultado da hipotenusa
+    cabecalho(f"Em um triângulo retângulo, com um cateto adjacente de {cor(ca)} unidades de medida \ne um cateto oposto de {cor(co)} unidades de medida, a hipotenusa, que é o lado oposto ao ângulo reto, \né calculada como a raiz quadrada da soma dos quadrados dos catetos. \nNo caso dado, a hipotenusa é igual a {cor(hipo)} unidades de medida.",quantC=100)
     
-    h=math.hypot(co,ca)
-    trianguloRetangulo[2].append(h)
-
-    print("=-"*30)
-    print(f"{color1}Cateto oposto: {trianguloRetangulo[0]}")
-    print(f"{color2}Cateto adjacente: {trianguloRetangulo[1]}")
-    print(f"{color3}Hipotenusa: {trianguloRetangulo[2]}{colorEnd}")
-    c2=True
-
-    print("=-"*30)
-    while c2:
-        print("=-"*30)
-        c=int(input("Deseja efetuar alguma alteração? \n1-Alterar o cateto oposto\n2-Alterar o cateto adjacente\n3-Resetar o programa\n4-Sair do programa\n"))
-        c2=False
-        print("=-"*30)
-
-        if c==1:
-            trianguloRetangulo[0].clear()
-            co=float(input(f"{color1}Digite o cateto oposto: {colorEnd}"))
-            trianguloRetangulo[0].append(co)
-            h=math.hypot(co,trianguloRetangulo[1][-1])
-            trianguloRetangulo[2][-1]=h
-            print(f"{color3}Hipotenusa: {trianguloRetangulo[2]}{colorEnd}")
-            c2=True
-            
-        elif c==2:
-            trianguloRetangulo[1].clear()
-            ca=float(input(f"{color2}Digite o cateto adjacente: {colorEnd}"))
-            trianguloRetangulo[1].append(ca)
-            h=math.hypot(ca,trianguloRetangulo[2][-1])
-            trianguloRetangulo[2][-1]=h
-            print(f"{color3}Hipotenusa: {trianguloRetangulo[2]}{colorEnd}")
-            c2=True
-            
-        elif c==3:
-            trianguloRetangulo[0].clear()
-            trianguloRetangulo[1].clear()
-            trianguloRetangulo[2].clear()
-        elif c==4:
-            print(f"{color1}Obrigado e tenha um bom dia!")
-            exit()
+    while True:
+        c=str(input("Deseja continuar? [S/N]")).upper()[0]
+        
+        if c in ["S","N"]:
+            break
+        else:
+            print(cor("Erro! Digite S para continuar o programa ou N para sair!",31))
+        
+    if c in "N":
+        print(cor("Obrigado e volte sempre!",35))
+        break
