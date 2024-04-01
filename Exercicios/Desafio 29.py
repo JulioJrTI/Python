6373b56be61d1db88918768e119b45d637876186
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-04-01 19:15:25 -0300

    Exercicio 29

--- Exercicios/Desafio 29.py
+++ Exercicios/Desafio 29.py
@@ -12,22 +12,30 @@ from Modulos.formatar import cabecalho,cor
 #Greetings
 cabecalho(cor("Bem vindo(a) ao speed-o-meter da Alexa Luzia!",35))
 
+#Criando uma função que irá multar (valor inserido como parametro) o usuario caso o mesmo tenha ultrapassado o limite indicado como parametro
+def multador_carro(kmPercorrido=0,limite=0,multa=0):
+    """
+    Parametros:
+    kmPercorrido = A quantidade de KMs percorridos pelo usuario
+    limite = Caso o usuario ultrapasse o limite inserido, o mesmo será multado
+    multa = Valor monetario para a multa (Será cobrado por cada KMs acima do limite)
+    
+    """
+    
+    if kmPercorrido > limite:
+        print(cor("Vc foi multado!",31))            
+        aPagar = float((kmPercorrido-80)*multa)
+        print(cor(f"KMs percorridos acima do limite: {kmPercorrido-80}KMs\nValor a pagar: R${aPagar:.2f}",31))
+    else:
+        print(cor(f"Tudo certo!\nKMs percorridos: {kmPercorrido}KMs"))
+    
 #Tratamento de erro
 while True:    
     try:
-        velCarro = int(input("Digite a velocidade percorrida pelo carro: "))
+        velocidadeCarro = int(input("Digite a velocidade percorrida pelo carro: "))
         break
     except ValueError:
         print(cor("Erro! Digite somente valores numericos!",31))
 
-if velCarro > 80:
-    print(cor("Vc foi multado!",31))
-    multa = 7    
-    aPagar = float((velCarro-80)*multa)
-    print(cor(f"KMs percorridos acima do limite: {velCarro-80}KMs\nValor a pagar: R${aPagar:.2f}",31))
-else:
-    print(cor(f"Tudo certo!\nKMs percorridos: {velCarro}KMs"))
-
-
-
-
+#Chamando da função criada
+multador_carro(velocidadeCarro,80,7)
