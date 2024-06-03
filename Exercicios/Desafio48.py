3669b83dfa2125a64a26b708aeb90deaa04d2477
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-06-03 20:38:23 -0300

    Exercicio 48

--- Exercicios/Desafio48.py
+++ Exercicios/Desafio48.py
@@ -1,16 +1,41 @@
-"""Desafio 48: Faça um programa que calcule a soma entre todos os numeros impares que são multiplos de tres e que se encontram no intervalo de 1 até 500."""
+"""Desafio 48: Faça um programa que calcule a soma entre todos os numeros impares 
+que são multiplos de tres e que se encontram no intervalo de 1 até 500."""
 
-color1="\033[1;32m"
-color2="\033[1;33m"
-colorEnd="\033[m"
+#Limpando o terminal a cada execução do programa
+from os import system
+system("cls")
 
-soma=0
-numeros={"Numeros impares e multiplos de tres":[]}
+#Usando de modulos para a formatação do programa
+from Modulos.formatar import cabecalho,cor
 
-for n in range(1,500):
-    if n%2==1 and n%3==0:
-        numeros["Numeros impares e multiplos de tres"].append(n)
-        soma+=n
-print(f"{color1}Numeros impares e multiplos de 3: {colorEnd}{numeros['Numeros impares e multiplos de tres']}")
-print(f"{color2}Soma total: {soma}{colorEnd}")
+#Greetings!
+cabecalho(cor("Bem vindo ao somador de numeros impares da Prof(a) Alexa!",35))
+
+#Criando uma função que irá somar e exibir numeros impares que são multiplos de 3
+def somar_numerosImpares(i=0,f=0,m=3):
+    
+    """
+    Iremos somar os numeros impares que são multiplos por 3
+    Parametros: i=(Numero inicial),f=(Numeros final),m=(Multiplos, por padrão será multiplos de 3)
+    
+    """
+    
+    if f==0:
+        print(cor("Erro! Digite um valor numerico final no segundo parametro!",31))
+    else:
+        #Variavel irá receber a soma de todos os numeros impares que são multiplos de tres
+        somaNumeros=0
+
+        print(cor(f"Numeros impares de {i} a {f} que são multiplos de {m} e seu resultado de soma: ",34,),end="")
         
+        #Usando loop FOR para calcularmos os numeros impares multiplos de tres
+        for num in range(i,f+1):
+            if num%2==1 and num%m==0:
+                print(num,end=" > ")
+                somaNumeros+=num
+
+        #Imprimindo resultado da soma        
+        print(f"\b\b= {cor(somaNumeros)}")
+    
+#Chamando da função criada
+somar_numerosImpares(1,500)        
--- Exercicios/Modulos/Matematica.py
+++ Exercicios/Modulos/Matematica.py
@@ -114,7 +114,33 @@ def print_num_pares(n=0,f=0):
         for num in range(n,f+1):
             if num%2==0:
                 print(num,end=" > ")
-        print("\b\b ") 
+        print("\b\b ")
+        
+#Criando uma função que irá somar e exibir numeros impares que são multiplos de 3
+def somar_numerosImpares(i=0,f=0,m=3):
+    
+    """
+    Iremos somar os numeros impares que são multiplos por 3
+    Parametros: i=(Numero inicial),f=(Numeros final),m=(Multiplos, por padrão será multiplos de 3)
+    
+    """
+    
+    if f==0:
+        print(cor("Erro! Digite um valor numerico final no segundo parametro!",31))
+    else:
+        #Variavel irá receber a soma de todos os numeros impares que são multiplos de tres
+        somaNumeros=0
+
+        print(cor(f"Numeros impares de {i} a {f} que são multiplos de {m} e seu resultado de soma: ",34,),end="")
+        
+        #Usando loop FOR para calcularmos os numeros impares multiplos de tres
+        for num in range(i,f+1):
+            if num%2==1 and num%m==0:
+                print(num,end=" > ")
+                somaNumeros+=num
+
+        #Imprimindo resultado da soma        
+        print(f"\b\b= {cor(somaNumeros)}") 
     
 #Criando uma função que irá imprimir se o ano (inserido como parametro) é ou não bissexto
 def ano_bissexto(ano=0): 
