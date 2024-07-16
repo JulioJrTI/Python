9d02395b825e8ead532f583c6d9c30a0432146ee
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-07-16 20:40:35 -0300

    Exercicio 58

--- ExerciciosPython/Desafio58.py
+++ ExerciciosPython/Desafio58.py
@@ -1,48 +1,45 @@
 """Desafio 58: Melhore o jogo do DESAFIO 28 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessarios para vencer."""
 
-from random import randint
+# Usando de modulos para a formatação do programa
+from Modulos.formatar import cabecalho,cor,limpar_terminal
+
+# Efeito cosmetico de loading
 from time import sleep
 
-c1="\033[1;32m"
-c2="\033[1;31m"
-c0="\033[m"
+# Usando da biblioteca random para o sorteio de numero aleatorio por parte da maquina
+from random import randint
 
-maquina=randint(0,10)
+# Limpando o terminal a cada execução do programa
+limpar_terminal()
 
-palpites=0
+# Rounds no game
+rounds_ate_vitoria = 0
 
-print(f"{c1}Bem vindo ao jogo de adivinhação da Alexa!{c0}")
+# Maquina irá sortear um numero aleatorio de 0 a 10
+maquina = randint(0,10)
+
+# Greetings!
+cabecalho(cor('Bem vindo(a) ao game de adivinhação da Prof(a) Alexa Luzia!',35))
+
+#Programa principal
 while True:
-    player=int(input("Digite um numero: "))
-    print("A maquina está pensando...")
-    sleep(3)
+   
+    # Jogador irá inserir um numero qualquer de 0 a 10    
+    while True:
+        try:
+            print(cor(f'Round {rounds_ate_vitoria+1}: '))
+            player=int(input('Digite um numero de 0 a 10: '))
+            break
+        except ValueError:
+            print(cor('ERRO! Digite somente valores numericos!',31))
+    
+    # Adicionando +1 a cada round até a vitoria do jogador
+    rounds_ate_vitoria+=1
 
-    print("-="*10)
-    print(f"Voce escolheu o numero {player}.")
-    sleep(1)    
-    print("-="*10)
-
-    if player==maquina:
-        palpites+=1
-        print(f"{c1}Vc ganhou!{c0}",end="")
-        print(f" e foram necessarias {palpites} tentativas para vencer.")
+    # Logica para a vitoria ou derrota
+    if player == maquina:
+        print(cor('\nPlayer ganhou!',34))
+        print(cor(f'\nForam necessarios {rounds_ate_vitoria} palpites até a vitoria.',35))
         break
     else:
-        palpites+=1
-        print(f"{c2}Vc perdeu!{c0}")
-
-    sleep(2)
-
-    while True:
-        c=str(input("Deseja tentar novamente? [S/N]")).upper()[0]
-
-        if c in ["S","N"]:
-            break
-        else:
-            print("Erro, por favor digite somente S ou N!")
-    
-    if c in "Nn":
-        print("Obrigado por jogar!")
-        break
-
-
+        print(cor('\nMaquina ganhou!\n',31))
